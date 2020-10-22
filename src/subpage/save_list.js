00ec2ca5fba97fd274e0257b0016ace6ade25778
HyeonSeok <ajy720@gmail.com>
2020-10-22 22:46:47 +0900

    export, delete, modify, reset button & style

--- src/subpage/save_list.js
+++ src/subpage/save_list.js
@@ -9,6 +9,11 @@ window.onload = () => {
     let keyword2_list = document.querySelector(".keyword2_list");
     let keyword3_list = document.querySelector(".keyword3_list");
 
+    let simpledownBtn = document.querySelector(".simpledown");
+    let changeBtn = document.querySelector(".change");
+    let deleteBtn = document.querySelector(".delete");
+    let resetBtn = document.querySelector(".reset");
+
     let keyword_list = document.querySelectorAll(".list_right > div");
     let i = 0;
     // for(i=0; i<keyword_list.length-1; i++) {
@@ -49,10 +54,27 @@ window.onload = () => {
         keyword3_list.style.display = "block";
     })
 
-    chrome.storage.sync.get("keyword", ({ keyword }) => {
-        copyToCliipBoard(keyword)
+    simpledownBtn.addEventListener("click", () => {
+        chrome.storage.sync.get("keyword", ({ keyword }) => {
+            copyToCliipBoard(keyword)
+        })
     })
 
+
+    changeBtn.addEventListener("click", () => {
+
+    })
+
+
+    deleteBtn.addEventListener("click", () => {
+
+    })
+
+    resetBtn.addEventListener("click", () => {
+        chrome.storage.sync.set({ "keyword": {} })
+        
+        alert("데이터가 초기화 되었습니다.")
+    })
 
     function copyToCliipBoard(keyword) {
         keys = Object.keys(keyword)
